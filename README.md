a4d01437dfd90bd0dd4e589961aa070806566c66
Kieran Mangan <kieran@mettle.co.uk>
2024-11-22 15:12:38 +0000

    Implement simple RAG pipeline

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # haystack-education
 
 WIP
+
+A generative document intelligence system with a Haystack RAG pipeline.
+
+## Requirements:
+
+Python3.8 (higher versions cause Haystack dependency issues)
+Your OpenAI API key as an enironment variable: OPENAI_API_KEY
--- main.py
+++ main.py
@@ -1,53 +1,72 @@
 import os
 from haystack import Pipeline, Document
-from haystack.utils import Secret
 from haystack.document_stores.in_memory import InMemoryDocumentStore
-from haystack.components.retrievers.in_memory import InMemoryBM25Retriever
 from haystack.components.generators import OpenAIGenerator
-from haystack.components.builders.answer_builder import AnswerBuilder
 from haystack.components.builders.prompt_builder import PromptBuilder
+from pathlib import Path
+from datasets import load_dataset
+from haystack.components.embedders import SentenceTransformersDocumentEmbedder
+from haystack.components.embedders import SentenceTransformersTextEmbedder
+from haystack.components.retrievers.in_memory import InMemoryEmbeddingRetriever
+from haystack.components.builders import PromptBuilder
+from getpass import getpass
+from haystack.components.generators import OpenAIGenerator
+
 
 def main():
-    # Write documents to InMemoryDocumentStore
-    document_store = InMemoryDocumentStore()
-    document_store.write_documents([
-        Document(content="My name is Jean and I live in Paris."), 
-        Document(content="My name is Mark and I live in Berlin."), 
-        Document(content="My name is Giorgio and I live in Rome.")
-    ])
 
-    # Build a RAG pipeline
-    prompt_template = """
-    Given these documents, answer the question.
-    Documents:
-    {% for doc in documents %}
-        {{ doc.content }}
+    # Initialise the InMemoryDocumentStore
+    document_store = InMemoryDocumentStore()
+
+    dataset = load_dataset("bilgeyucel/seven-wonders", split="train")
+    docs = [Document(content=doc["content"], meta=doc["meta"]) for doc in dataset]
+
+
+    doc_embedder = SentenceTransformersDocumentEmbedder(model="sentence-transformers/all-MiniLM-L6-v2")
+    doc_embedder.warm_up()
+
+    docs_with_embeddings = doc_embedder.run(docs)
+    document_store.write_documents(docs_with_embeddings["documents"])
+
+    text_embedder = SentenceTransformersTextEmbedder(model="sentence-transformers/all-MiniLM-L6-v2")
+    retriever = InMemoryEmbeddingRetriever(document_store)
+
+
+    template = """
+    Given the following information, answer the question.
+
+    Context:
+    {% for document in documents %}
+        {{ document.content }}
     {% endfor %}
+
     Question: {{question}}
     Answer:
     """
 
-    retriever = InMemoryBM25Retriever(document_store=document_store)
-    prompt_builder = PromptBuilder(template=prompt_template)
-    llm = OpenAIGenerator()
+    prompt_builder = PromptBuilder(template=template)
 
-    rag_pipeline = Pipeline()
-    rag_pipeline.add_component("retriever", retriever)
-    rag_pipeline.add_component("prompt_builder", prompt_builder)
-    rag_pipeline.add_component("llm", llm)
-    rag_pipeline.connect("retriever", "prompt_builder.documents")
-    rag_pipeline.connect("prompt_builder", "llm")
+    if "OPENAI_API_KEY" not in os.environ:
+        os.environ["OPENAI_API_KEY"] = getpass("Enter OpenAI API key:")
+    generator = OpenAIGenerator(model="gpt-4o-mini")
 
-    # Ask a question
-    question = "Who lives in Paris?"
-    results = rag_pipeline.run(
-        {
-            "retriever": {"query": question},
-            "prompt_builder": {"question": question},
-        }
-    )
+    basic_rag_pipeline = Pipeline()
+    # Add components to your pipeline
+    basic_rag_pipeline.add_component("text_embedder", text_embedder)
+    basic_rag_pipeline.add_component("retriever", retriever)
+    basic_rag_pipeline.add_component("prompt_builder", prompt_builder)
+    basic_rag_pipeline.add_component("llm", generator)
 
-    print(results["llm"]["replies"])
+    # Now, connect the components to each other
+    basic_rag_pipeline.connect("text_embedder.embedding", "retriever.query_embedding")
+    basic_rag_pipeline.connect("retriever", "prompt_builder.documents")
+    basic_rag_pipeline.connect("prompt_builder", "llm")
 
+    question = "What does Rhodes Statue look like?"
+
+    response = basic_rag_pipeline.run({"text_embedder": {"text": question}, "prompt_builder": {"question": question}})
+
+    print(response["llm"]["replies"][0])
+  
 if __name__ == "__main__":
     main()
